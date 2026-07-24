11650249289f8d13b472c227209c6ae14a4b6e4f
tallestgabs <gabrielcastrodias1414@gmail.com>
2026-07-24 13:52:11 -0300

    add zoom no hero-banner

--- src/_components/HeroBanner.tsx
+++ src/_components/HeroBanner.tsx
@@ -3,12 +3,14 @@ import heroBanner from "~/assets/hero-banner.jpg";
 
 export function HeroBanner() {
   return (
-    <section className="w-full">
-      <Image
-        src={heroBanner}
-        alt="Banner Eletrostruct"
-        className="h-auto w-full object-cover"
-      />
+    <section className="max-w-8xl mx-auto w-full px-6 py-8">
+      <div className="relative aspect-[21/9] w-full overflow-hidden rounded-2xl">
+        <Image
+          src={heroBanner}
+          alt="Banner Eletrostruct"
+          className="h-auto w-full rounded-2xl object-cover transition-transform duration-500 hover:scale-103"
+        />
+      </div>
     </section>
   );
 }
