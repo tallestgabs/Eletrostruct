fb4ad2a85c38893412a242316b49eed62bd6fd3c
tallestgabs <gabrielcastrodias1414@gmail.com>
2026-07-24 13:53:55 -0300

    remove escrita com useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,47 +7,10 @@ import { Footer } from "~/_components/Footer";
 import { HeroBanner } from "~/_components/HeroBanner";
 
 export default function HomePage() {
-  const [typingText, setTypingText] = useState("");
-  const CompleteText = "Os Melhores Preços do Mercado!";
-
-  useEffect(() => {
-    setTypingText(""); //comeca vazio
-    let i = 0;
-
-    const intervalId = setInterval(() => {
-      if (i < CompleteText.length) {
-        // adiciona proximo character
-        setTypingText((previousText) => previousText + CompleteText.charAt(i));
-        i++;
-      } else {
-        clearInterval(intervalId); // limpa quando termina o texto
-      }
-    }, 50); // 50 ms de velocidade
-
-    // função de limpeza
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []); // roda apenas uma vez ao montar o componente
-
   return (
-    <main className="min-h-screen bg-gradient-to-b from-green-700 via-green-700 to-black">
+    <main className="bg-background flex min-h-screen flex-col">
       <Navbar />
       <HeroBanner />
-      <div className="flex flex-col items-center justify-center pt-32">
-        <h1 className="text-center text-[150px] text-white/70">
-          <span className="text-green-400">{"{"}</span>
-          Eletrostruct
-          <span className="text-green-400">{"}"}</span>
-        </h1>
-        <p className="text-[25px] text-white/60">
-          {typingText}
-          <span className="ml-1 animate-pulse font-light text-yellow-100">
-            |
-          </span>
-        </p>
-      </div>
-
       <Footer />
     </main>
   );
