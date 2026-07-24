9906d3a2ba11824ba19ad28082d978ae067d517d
tallestgabs <gabrielcastrodias1414@gmail.com>
2026-07-24 14:03:43 -0300

    muda cores do footer

--- src/_components/Footer.tsx
+++ src/_components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="inset -x-0 fixed bottom-0 flex w-full justify-center bg-black p-1 text-white/60">
+    <footer className="inset -x-0 --background text-foreground fixed bottom-0 flex w-full justify-center p-1">
       <h1 className="p-2 text-[40px]">{"{Eletrostruct}"}</h1>
       <p className="p-6">Copyright © 2026 | Created by Us</p>
     </footer>
